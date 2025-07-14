8d03f6b0e1638a886b7d289a0b281485fd78f653
David Phillips <davidphillips@Davids-MacBook-Air.local>
2025-07-14 13:11:03 -0400

    Made top nav sticky

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -238,7 +238,7 @@ useEffect(() => {
   };
   
   return (
-    <header className="bg-white shadow-sm relative">
+    <header className="bg-white shadow-sm sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
